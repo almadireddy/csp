624ad4861b62c189d4fe4abdf0154e44a6e6bbb6
Al Madireddy <al.madireddy@gmail.com>
2019-03-09 16:45:43 -0600

    completed most constrained and most constraining heuristics, still need forward checking and domain ordering

--- Problem.py
+++ Problem.py
@@ -5,6 +5,7 @@ class Problem:
         self.forward_checking = forward_checking
         self.call_depth = 0
 
+
     # add a variable to solve for
     def addVariable(self, variable, domain):
         if variable not in self.variables:
@@ -12,10 +13,12 @@ class Problem:
         else:
             print "u dumb, that's a duplicate variable im skipping."
 
+
     # add a constraint function for variables
     # constraint looks like (constraintFunction, variables to constrain)
     def addConstraint(self, constraint_function, variables):
         self.constraints.append((constraint_function, variables))
+
 
     # call to start the solver
     # return dictionary of assignments or false
@@ -26,6 +29,7 @@ class Problem:
         else:
             return False
 
+
     # the recursive part of the solver
     # performs backtracking search
     # this maps almost exactly to pseudocode in slides
@@ -33,8 +37,8 @@ class Problem:
         # base case
         if len(assignments) == len(self.variables):
             if self.goalTest(assignments):
-                if self.call_depth < 30:
-                    self.print_assignments(assignments, 'solution')
+                # if self.call_depth < 30:
+                self.print_assignments(assignments, 'solution')
                 return assignments
             else:
                 return False
@@ -46,52 +50,83 @@ class Problem:
                 result = self.solve_recursive(assignments)
 
                 if result is not False:
+                    remaining_domains = self.variables[var][1]
+                    constraints = self.get_constraints(var)
+
                     return result
 
-                if self.call_depth < 30:
-                    self.print_assignments(assignments, 'failure')
-                    self.call_depth += 1
+                # if self.call_depth < 30:
+                self.print_assignments(assignments, 'failure')
+                self.call_depth += 1
 
                 assignments.pop(var)
 
         return False
 
+
     def select_unassigned_variable(self, assignments):
-        # TODO: finish this algorithm that will select the variable to assign next,
-        #       based on the most constrained variable heuristic
-        #       and break ties with the most constraining variable heuristic
+        unassigned = self.get_unassigned(assignments)
+        valid_constraints = []
 
-        # There is a spot to store the remaining possible domain values built into the object stored
-        # in the self.variables which is a dictionary of
-        # variable name mapped to these: (domain, remaining Domain), modify only that second one
-        unassigned = []
-        for a in self.variables:
-            if a not in assignments:
-                unassigned.append(a)
+        to_pick_from = []
 
-        return unassigned[0]    # return the first unassigned variable for now
+        for u in unassigned:
+            constraints = self.get_constraints(u)
+
+            for c in constraints:
+                if c[1][0] in assignments or c[1][1] in assignments:
+                    valid_constraints.append(c)
+
+            new_remaining_domain = []
+            remaining_domain = self.variables[u][1]
+
+            for val in remaining_domain:
+                if self.check_consistency(u, val, assignments):
+                    new_remaining_domain.append(val)
+
+            to_pick_from.append([u, new_remaining_domain, 0])
+
+        to_pick_from.sort(key=lambda t: len(t[1]))
+
+        # the most constraining out of these
+        to_tie_break = []
+        min_val = len(to_pick_from[0][1])
+
+        for a in to_pick_from:
+            if len(a[1]) is min_val:
+                to_tie_break.append(a)
+
+        for var in to_tie_break:
+            unassigned_constraints = []
+            constraints = self.get_constraints(var[0])
+
+            for c in constraints:
+                if c[1][0] not in assignments and c[1][1] not in assignments:
+                    unassigned_constraints.append(c)
+
+            var[2] = len(unassigned_constraints)
+
+        to_tie_break.sort(key=lambda t: int(t[2]), reverse=True)
+
+        return to_tie_break[0][0]
+
 
 
     def order_domain_values(self, var):
         # self.variables[var] is (domain, remainingDomain)
         # we return the remaining domain
         # (for now just as-is without ordering)
-        constraints = []    # the constraints involving this variable
+        constraints = self.get_constraints(var)
+        domains = self.variables[var][1]
 
-        for a in self.constraints:
-            if var in a[1]:  # if the variable is in the listed arguments for the constraint
-                constraints.append(a)
-        # TODO: finish this algorithm that will order the domain values,
-        #       which are contained in self.variables[var][1]
+        to_pick_from = []
+
         return self.variables[var][1]
+
 
     def check_consistency(self, var, value, assignments):
         # this will check whether the assigned value is consistent
-        constraints = []
-
-        for a in self.constraints:
-            if var in a[1]:             # if the variable is in the listed arguments for the constraint
-                constraints.append(a)
+        constraints = self.get_constraints(var)
 
         for con in constraints:             # for each constraint involving this variable
             index_of_var = con[1].index(var)
@@ -99,7 +134,7 @@ class Problem:
             other_var = con[1][index_of_other_var]
 
             if con[1][1] in assignments:
-                to_pass = []
+                to_pass = [0, 0]
                 to_pass[index_of_var] = value
                 to_pass[index_of_other_var] = assignments[other_var]
                 if not con[0](to_pass):   # check if value is consistent
@@ -124,6 +159,25 @@ class Problem:
                 return False                    # if constraint doesnt hold return false
 
         return True
+
+
+    def get_constraints(self, var):
+        constraints = []  # the constraints involving this variable
+
+        for a in self.constraints:
+            if var in a[1]:  # if the variable is in the listed arguments for the constraint
+                constraints.append(a)
+
+        return constraints
+
+
+    def get_unassigned(self, assignments):
+        unassigned = []
+        for a in self.variables:
+            if a not in assignments:
+                unassigned.append(a)
+
+        return unassigned
 
 
     def print_assignments(self, assignments, status):
